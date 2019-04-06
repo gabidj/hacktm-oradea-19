5795c917ecac49b0a7ccf6004d5b8d0d49016c3d
MarioRadu <magda_marior@yahoo.com>
2019-04-06 22:22:26 +0300

    css fix

--- public/js/maps.js
+++ public/js/maps.js
@@ -1,4 +1,6 @@
 var data;
+var ui;
+var behavior;
 console.log($("#map-container"));
 
 function moveMapToBerlin(map){
@@ -7,10 +9,9 @@ function moveMapToBerlin(map){
 }
 
 function addMarkersToMap(map) {
-    for (var i = 0; i < sportWithVenues.length; i++) {
-        console.log(data.data.venues[i].name);
-        var venue = data.data.venues[i];
-        addMarker(map, {type:data.data.name, lat:venue.latitude, long:venue.longitude,
+    for (var i = 0; i < data.sportWithVenues.venues.length; i++) {
+        var venue = data.sportWithVenues.venues[i];
+        addMarker(map, {type:data.sportWithVenues.name, lat:venue.latitude, long:venue.longitude,
             name:venue.name, price:venue.price, startHour: venue.startHour,
             endHour: venue.endHour});
     }
@@ -36,7 +37,6 @@ function addMarker(map, info) {
     }
 
     domElement.className = "marker";
-
 
     //interaction
     function changeOpacity(evt) {
@@ -91,93 +91,44 @@ function addMarker(map, info) {
 }
 
 
-//API
-var request = new XMLHttpRequest()
 
-request.open('GET', 'http://hacktm.local/api/category/football', true)
+$('document').ready(function(){
 
-request.onload = function() {
-    // Begin accessing JSON data here
-    var api = JSON.parse(this.response)
-    if (request.status >= 200 && request.status < 400) {
-        data = api;
-        console.log(data);
-        addMarkersToMap(map);
-    } else {
-        console.log('error')
-    }
-}
-
-
-request.send()
-
-
-
-var platform = new H.service.Platform({
-    'app_id': '6YWqAz92yiVW4TmBg3ap',
-    'app_code': '2Njcxr7YQlMlNSSgJwckjQ'
-});
+    var platform = new H.service.Platform({
+        'app_id': '6YWqAz92yiVW4TmBg3ap',
+        'app_code': '2Njcxr7YQlMlNSSgJwckjQ'
+    });
 
 // Obtain the default map types from the platform object:
-var defaultLayers = platform.createDefaultLayers();
+    var defaultLayers = platform.createDefaultLayers();
 
-// Instantiate (and display) a map object:
-$('document').ready(function(){
     var map = new H.Map(document.getElementById('map-container'), defaultLayers.normal.map);
+    behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+    ui = H.ui.UI.createDefault(map, defaultLayers);
 
-    var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+    //API
+    var request = new XMLHttpRequest();
 
-    var ui = H.ui.UI.createDefault(map, defaultLayers);
+    request.open('GET', 'http://hacktm.local/api/category/football', true);
 
+    request.onload = function() {
+        // Begin accessing JSON data here
+        var api = JSON.parse(this.response)
+        if (request.status >= 200 && request.status < 400) {
+            data = api;
+            console.log(data);
+            addMarkersToMap(map);
+        } else {
+            console.log('error')
+        }
+    }
+
+    request.send();
 
     moveMapToBerlin(map);
-//addMarkersToMap(map);
-
     $('head').append('<link rel="stylesheet" href="https://js.api.here.com/v3/3.0/mapsjs-ui.css" type="text/css" />');
-
-
 
 });
 
-// /**
-//  * Moves the map to display over Berlin
-//  *
-//  * @param  {H.Map} map      A HERE Map instance within the application
-//  */
-// function moveMapToBerlin(map){
-//   map.setCenter({lat:52.5159, lng:13.3777});
-//   map.setZoom(14);
-// }
-//
 
-
-
-//
-// /**
-//  * Boilerplate map initialization code starts below:
-//  */
-//
-// //Step 1: initialize communication with the platform
-// var platform = new H.service.Platform({
-//   app_id: 'DemoAppId01082013GAL',
-//   app_code: 'AJKnXv84fjrb0KIHawS0Tg',
-//   useCIT: true,
-//   useHTTPS: true
-// });
-// var defaultLayers = platform.createDefaultLayers();
-// console.log(document.getElementById('map-container'))
-// //Step 2: initialize a map  - not specificing a location will give a whole world view.
-// var map = new H.Map(document.getElementById('map-container'), defaultLayers.normal.map);
-//
-// //Step 3: make the map interactive
-// // MapEvents enables the event system
-// // Behavior implements default interactions for pan/zoom (also on mobile touch environments)
-// var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
-//
-// // Create the default UI components
-// var ui = H.ui.UI.createDefault(map, defaultLayers);
-//
-// // Now use the map as required...
-// moveMapToBerlin(map);
-//
-// $('head').append('<link rel="stylesheet" href="https://js.api.here.com/v3/3.0/mapsjs-ui.css" type="text/css" />');
+// $('.H_ib_body').css({'background':'white'});​
