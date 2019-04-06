72b51c504ea8d2d293e49b2ad94c50d853675b9a
Gabi DJ (dev) <git@gabisuciu.ro>
2019-04-06 16:32:07 +0300

    begin appointment

---
+++ src/HackTM/src/Controller/AppointmentControllerFactory.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mario
+ * Date: 4/6/2019
+ * Time: 1:31 PM
+ */
+
+namespace Oradea\HackTM\Controller;
+use Doctrine\ORM\EntityManager;
+use Dot\Authentication\Web\Options\WebAuthenticationOptions;
+use Dot\Helpers\Route\RouteHelper;
+use Dot\User\Options\UserOptions;
+use Oradea\HackTM\Service\HackTmService;
+use Psr\Container\ContainerInterface;
+
+
+class AppointmentControllerFactory
+{
+    public function __invoke(ContainerInterface $container)
+    {
+        $config = $container->get("config");
+        $service = $container->get(HackTmService::class);
+        $options = $container->get(UserOptions::class);
+        $webAuthenticationOptions = $container->get(WebAuthenticationOptions::class);
+        $routeHelper = $container->get(RouteHelper::class);
+        $entityManager = $container->get(EntityManager::class);
+
+        return new AppointmentController(
+            $config,
+            $service,
+            $options,
+            $webAuthenticationOptions,
+            $routeHelper,
+            $entityManager
+        );
+    }
+}
